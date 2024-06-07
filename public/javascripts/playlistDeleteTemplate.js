19e8377d29707062be997b6d4ea69145bf4e1dc2
Carson Secrest <carsonsecrest@gmail.com>
2024-06-07 15:53:26 -0700

    Clean up delete playlist partial UI

--- public/javascripts/playlistDeleteTemplate.js
+++ public/javascripts/playlistDeleteTemplate.js
@@ -8,7 +8,7 @@ templates['playlist_del.hbs'] = template({"1":function(container,depth0,helpers,
         return undefined
     };
 
-  return "        <div>\r\n          <input type=\"radio\" id=\""
+  return "        <div class='w-full justify-start flex flex-row space-x-2 items-center'>\r\n          <input type=\"radio\" id=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"playlist_id") || (depth0 != null ? lookupProperty(depth0,"playlist_id") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"playlist_id","hash":{},"data":data,"loc":{"start":{"line":8,"column":34},"end":{"line":8,"column":49}}}) : helper)))
     + "\" name=\"playlist_id\" value=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"playlist_id") || (depth0 != null ? lookupProperty(depth0,"playlist_id") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"playlist_id","hash":{},"data":data,"loc":{"start":{"line":8,"column":77},"end":{"line":8,"column":92}}}) : helper)))
@@ -27,8 +27,8 @@ templates['playlist_del.hbs'] = template({"1":function(container,depth0,helpers,
         return undefined
     };
 
-  return "<div class='flex justify-center items-center relative'>\r\n  <form action=\"/playlist/delete\" method=\"post\">\r\n    <h3 class='font-bold text-2xl' >Delete playlist</h3>\r\n    <fieldset>\r\n      <legend>Playlists:</legend>\r\n"
+  return "<div class='flex justify-center items-center relative'>\r\n  <form action=\"/playlist/delete\" method=\"post\">\r\n    <h3 class='font-bold text-2xl mb-3'>Delete playlist</h3>\r\n    <fieldset class='mb-4'>\r\n      <h4 class='text-xl text-center flex flex-col w-full mb-2'>Your playlists:</h4>\r\n"
     + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"playlists") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":6},"end":{"line":11,"column":15}}})) != null ? stack1 : "")
-    + "    </fieldset>\r\n    <div>\r\n      <input type=\"submit\" value=\"Delete\" />\r\n    </div>\r\n  </form>\r\n</div>";
+    + "    </fieldset>\r\n    <div>\r\n      <button type=\"submit\" value=\"Delete\" class=\"text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:bg-red-400 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800\" >Delete</button>\r\n    </div>\r\n  </form>\r\n</div>";
 },"useData":true});
 })();
